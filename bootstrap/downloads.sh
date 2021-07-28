3d773ff4f5720a39a50b293fe760ada4ddb50521
matanhaz <48633383+matanhaz@users.noreply.github.com>
2021-07-28 16:08:52 +0300

    Update downloads.sh

--- bootstrap/downloads.sh
+++ bootstrap/downloads.sh
@@ -14,8 +14,8 @@ wget -nc -O archives/Commons/COLLECTIONS.tar https://sourceforge.net/projects/ir
 # wget -nc -O archives/Commons/CRYPTO.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/CRYPTO.tar
 # wget -nc -O archives/Commons/CSV.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/CSV.tar
 # wget -nc -O archives/Commons/IO.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/IO.tar
-# wget -nc -O archives/Commons/LANG.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/LANG.tar
-# wget -nc -O archives/Commons/MATH.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/MATH.tar
+wget -nc -O archives/Commons/LANG.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/LANG.tar
+wget -nc -O archives/Commons/MATH.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/MATH.tar
 # wget -nc -O archives/Commons/WEAVER.tar https://sourceforge.net/projects/irblsensitivity/files/Commons/WEAVER.tar
 
 mkdir -p archives/JBoss
