afdbeed0f498f61675eb09e6a8cecc959b0bf819
matanhaz <48633383+matanhaz@users.noreply.github.com>
2022-04-10 17:08:07 +0300

    Update FileParser.java

--- src/BugLocator/src/org/buglocator/sourcecode/ast/FileParser.java
+++ src/BugLocator/src/org/buglocator/sourcecode/ast/FileParser.java
@@ -16,23 +16,13 @@ public class FileParser {
 
 	private CompilationUnit cu = null;
 
-	/**
-	 * 占쏙옙占쌍몌옙占쏙옙占퐅ava占식쇽옙占쏙옙迦占쏙옙CompilationUnit
-	 * 
-	 * @param file:java 占식쇽옙
-	 * 
-	 */
 	public FileParser(File file) {
 		ASTCreator creator = new ASTCreator();
 		creator.getFileContent(file);
 		cu = creator.getCompilationUnit();
 	}
 
-	/**
-	 * 占쏙옙혤java占식쇽옙占식댐옙占쏙옙占쏙옙占쏙옙
-	 * 
-	 * @return 占쏙옙占쏙옙占쏙옙占쏙옙
-	 */
+	
 	public int getLinesOfCode() {
 		this.deleteNoNeededNode();
 		String[] lines = cu.toString().split("\n");
@@ -46,11 +36,6 @@ public class FileParser {
 		return len;
 	}
 
-	/**
-	 * 占쏙옙혤占쏙옙占쏙옙캇占쏙옙캔占쏙옙占�
-	 * 
-	 * @return 占쏙옙占쏙옙캇占쏙옙캔占쏙옙占쏙옙占쏙옙占�
-	 */
 	public String[] getContent() {
 		String[] tokensInSourceCode = Splitter.splitSourceCode(this.deleteNoNeededNode());
 		StringBuffer sourceCodeContentBuffer = new StringBuffer();
@@ -66,21 +51,13 @@ public class FileParser {
 		return content.split(" ");
 	}
 
-	/**
-	 * 占쏙옙혤占식쇽옙占쏙옙占쌘곤옙占쏙옙
-	 * 
-	 * @return 占쏙옙占쏙옙
-	 */
+	
 	public String getPackageName() {
 
 		return cu.getPackage() == null ? "" : cu.getPackage().getName().getFullyQualifiedName();
 	}
 
-	/**
-	 * 占쏙옙혤占식쇽옙占싻듸옙占쏙옙占싻뤄옙占쏙옙占쏙옙
-	 * 
-	 * @return 占쏙옙占쏙옙占쏙옙占쏙옙占쏙옙囹占�
-	 */
+	
 	private String getAllMethodName() {
 		ArrayList<String> methodNameList = new ArrayList<String>();
 		for (int i = 0; i < cu.types().size(); i++) {
@@ -99,11 +76,7 @@ public class FileParser {
 
 	}
 
-	/**
-	 * 占쏙옙혤占식쇽옙占싻듸옙占쏙옙占쏙옙占쏙옙占쏙옙
-	 * 
-	 * @return 占쏙옙占쏙옙占쏙옙占쏙옙囹占�
-	 */
+	
 	private String getAllClassName() {
 		ArrayList<String> classNameList = new ArrayList<String>();
 		for (int i = 0; i < cu.types().size(); i++) {
@@ -118,11 +91,7 @@ public class FileParser {
 		return allClassName.trim();
 	}
 
-	/**
-	 * 占쏙옙占식쇽옙占싻뀐옙占쏙옙狼占쏙옙占쏙옙口
-	 * 
-	 * @return 占식쇽옙占쏙옙占쌍뤄옙占십�
-	 */
+	
 	private String deleteNoNeededNode() {
 		cu.accept(new ASTVisitor() {
 			public boolean visit(AnnotationTypeDeclaration node) {
