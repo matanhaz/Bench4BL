ad4c4f9c2470d4f244e844ab1eee4ce2150b4428
matanhaz <48633383+matanhaz@users.noreply.github.com>
2022-04-10 17:05:37 +0300

    Update FileParser.java

--- src/BRTracer/src/org/brtracer/sourcecode/ast/FileParser.java
+++ src/BRTracer/src/org/brtracer/sourcecode/ast/FileParser.java
@@ -1,3 +1,45 @@
+// package org.brtracer.sourcecode.ast;
+
+// import java.io.*;
+// import java.util.ArrayList;
+
+// import org.brtracer.utils.Splitter;
+// import org.eclipse.jdt.core.dom.ASTVisitor;
+// import org.eclipse.jdt.core.dom.AnnotationTypeDeclaration;
+// import org.eclipse.jdt.core.dom.CompilationUnit;
+// import org.eclipse.jdt.core.dom.ImportDeclaration;
+// import org.eclipse.jdt.core.dom.MethodDeclaration;
+// import org.eclipse.jdt.core.dom.PackageDeclaration;
+// import org.eclipse.jdt.core.dom.TypeDeclaration;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// public class FileParser {
+
+// 	private CompilationUnit cu = null;
+
+
+// 	public FileParser(File file) {
+// 		ASTCreator creator = new ASTCreator();
+// 		creator.getFileContent(file);
+// 		cu = creator.getCompilationUnit();
+// 	}
+
+	
+// 	public int getLinesOfCode() {
+// 		this.deleteNoNeededNode();
+// 		String[] lines = cu.toString().split("\n");
+// 		int len = 0;
+// 		for (String strLine : lines) {
+// 			if (!strLine.trim().equals("")) {
+// 				len++;
+
+// 			}
+// 		}
+// 		return len;
+// 	}
+
 package org.brtracer.sourcecode.ast;
 
 import java.io.*;
@@ -12,14 +54,11 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.PackageDeclaration;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class FileParser {
 
 	private CompilationUnit cu = null;
 
-
+	
 	public FileParser(File file) {
 		ASTCreator creator = new ASTCreator();
 		creator.getFileContent(file);
@@ -51,36 +90,12 @@ public class FileParser {
 		String content = sourceCodeContentBuffer.toString().toLowerCase();
 		return content.split(" ");
 	}
-	public String getContentAsString(){
-		String[] tokensInSourceCode = Splitter.splitSourceCode(this.deleteNoNeededNode());
-		StringBuffer sourceCodeContentBuffer = new StringBuffer();
-		for (String token : tokensInSourceCode) {
-			sourceCodeContentBuffer.append(token + " ");
-		}
-		String content = sourceCodeContentBuffer.toString().toLowerCase();
-		return content;
-	}
 
 	public String[] getClassNameAndMethodName() {
 		String content = (this.getAllClassName() + " " + this
 				.getAllMethodName()).toLowerCase();
 		return content.split(" ");
 	}
-	
-	public  MethodDeclaration[] getAllMethods() {
-		List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
-		for (int i = 0; i < cu.types().size(); i++) {
-			TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
-			MethodDeclaration[] methodDecls = type.getMethods();
-			for (MethodDeclaration methodDecl : methodDecls) {
-				methods.add(methodDecl);
-			}
-		}
-		//return methods.toArray();
- 		return methods.toArray(new MethodDeclaration[0]);
-	}
-	
-	
 
 	
 	public String getPackageName() {
@@ -89,6 +104,7 @@ public class FileParser {
 				.getFullyQualifiedName();
 	}
 
+	
 	private String getAllMethodName() {
 		ArrayList<String> methodNameList = new ArrayList<String>();
 		for (int i = 0; i < cu.types().size(); i++) {
@@ -109,7 +125,7 @@ public class FileParser {
 	}
 
 	
-	public String getAllClassName() {
+	private String getAllClassName() {
 		ArrayList<String> classNameList = new ArrayList<String>();
 		for (int i = 0; i < cu.types().size(); i++) {
 			TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
@@ -163,3 +179,127 @@ public class FileParser {
 		});
 	}
 }
+
+
+// 	public String[] getContent() {
+// 		String[] tokensInSourceCode = Splitter.splitSourceCode(this
+// 				.deleteNoNeededNode());
+// 		StringBuffer sourceCodeContentBuffer = new StringBuffer();
+// 		for (String token : tokensInSourceCode) {
+// 			sourceCodeContentBuffer.append(token + " ");
+// 		}
+// 		String content = sourceCodeContentBuffer.toString().toLowerCase();
+// 		return content.split(" ");
+// 	}
+// 	public String getContentAsString(){
+// 		String[] tokensInSourceCode = Splitter.splitSourceCode(this.deleteNoNeededNode());
+// 		StringBuffer sourceCodeContentBuffer = new StringBuffer();
+// 		for (String token : tokensInSourceCode) {
+// 			sourceCodeContentBuffer.append(token + " ");
+// 		}
+// 		String content = sourceCodeContentBuffer.toString().toLowerCase();
+// 		return content;
+// 	}
+
+// 	public String[] getClassNameAndMethodName() {
+// 		String content = (this.getAllClassName() + " " + this
+// 				.getAllMethodName()).toLowerCase();
+// 		return content.split(" ");
+// 	}
+	
+// 	public  MethodDeclaration[] getAllMethods() {
+// 		List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
+// 		for (int i = 0; i < cu.types().size(); i++) {
+// 			TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
+// 			MethodDeclaration[] methodDecls = type.getMethods();
+// 			for (MethodDeclaration methodDecl : methodDecls) {
+// 				methods.add(methodDecl);
+// 			}
+// 		}
+// 		//return methods.toArray();
+//  		return methods.toArray(new MethodDeclaration[0]);
+// 	}
+	
+	
+
+	
+// 	public String getPackageName() {
+
+// 		return cu.getPackage() == null ? "" : cu.getPackage().getName()
+// 				.getFullyQualifiedName();
+// 	}
+
+// 	private String getAllMethodName() {
+// 		ArrayList<String> methodNameList = new ArrayList<String>();
+// 		for (int i = 0; i < cu.types().size(); i++) {
+// 			TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
+// 			MethodDeclaration[] methodDecls = type.getMethods();
+// 			for (MethodDeclaration methodDecl : methodDecls) {
+// 				String methodName = methodDecl.getName()
+// 						.getFullyQualifiedName();
+// 				methodNameList.add(methodName);
+// 			}
+// 		}
+// 		String allMethodName = "";
+// 		for (String methodName : methodNameList) {
+// 			allMethodName += methodName + " ";
+// 		}
+// 		return allMethodName.trim();
+
+// 	}
+
+	
+// 	public String getAllClassName() {
+// 		ArrayList<String> classNameList = new ArrayList<String>();
+// 		for (int i = 0; i < cu.types().size(); i++) {
+// 			TypeDeclaration type = (TypeDeclaration) cu.types().get(i);
+// 			String name = type.getName().getFullyQualifiedName();
+// 			classNameList.add(name);
+// 		}
+// 		String allClassName = "";
+// 		for (String className : classNameList) {
+// 			allClassName += className + " ";
+// 		}
+// 		return allClassName.trim();
+// 	}
+
+	
+// 	private String deleteNoNeededNode() {
+// 		cu.accept(new ASTVisitor() {
+// 			public boolean visit(AnnotationTypeDeclaration node) {
+// 				if (node.isPackageMemberTypeDeclaration()) {
+
+// 					node.delete();
+// 				}
+// 				return super.visit(node);
+// 			}
+// 		});
+// 		cu.accept(new ASTVisitor() {
+// 			public boolean visit(PackageDeclaration node) {
+// 				node.delete();
+// 				return super.visit(node);
+// 			}
+// 		});
+// 		cu.accept(new ASTVisitor() {
+// 			public boolean visit(ImportDeclaration node) {
+// 				node.delete();
+// 				return super.visit(node);
+// 			}
+// 		});
+// 		return cu.toString();
+// 	}
+
+// 	public void getImport(final FileWriter writeImport){
+// 		cu.accept(new ASTVisitor() {
+// 			@Override
+// 			public boolean visit(ImportDeclaration node) {
+// 				try {
+// 					writeImport.write(node.getName() + " ");
+// 				} catch (IOException e) {
+// 					e.printStackTrace();
+// 				}
+// 				return super.visit(node);
+// 			}
+// 		});
+// 	}
+// }
