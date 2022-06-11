a5f9fad724a06edd21aaaa95311472906188a5bf
matanhaz <48633383+matanhaz@users.noreply.github.com>
2022-06-11 11:34:24 +0300

    Update Subjects.py

--- scripts/commons/Subjects.py
+++ scripts/commons/Subjects.py
@@ -22,7 +22,7 @@ class Subjects(object):
 	groups = ['Apache', 'Commons', 'JBoss', 'Wildfly', 'Spring']
 	projects = {
 		'Apache':['CAMEL', 'HBASE', 'HIVE', 'MAVEN', 'WICKET'],
-		'Commons':['CODEC', 'COLLECTIONS', 'COMPRESS', 'CONFIGURATION', 'CRYPTO', 'IO', 'LANG', 'MATH', 'WEAVER','CSV', 'CLI', 'NET'],
+		'Commons':['CODEC', 'COLLECTIONS', 'COMPRESS', 'CONFIGURATION', 'CRYPTO', 'IO', 'LANG', 'MATH', 'WEAVER','CSV', 'CLI', 'NET', 'IMAGING'],
 		'JBoss':['ENTESB', 'JBMETA'],
 		'Wildfly':['ELY', 'WFARQ', 'WFCORE', 'WFLY', 'WFMP','SWARM'],
 		'Spring':['AMQP', 'ANDROID', 'BATCH', 'BATCHADM', 'DATACMNS', 'DATAGRAPH', 'DATAJPA', 'DATAMONGO', 'DATAREDIS', 'DATAREST', 'LDAP', 'MOBILE', 'ROO', 'SEC', 'SECOAUTH', 'SGF', 'SHDP', 'SHL', 'SOCIAL', 'SOCIALFB', 'SOCIALLI', 'SOCIALTW', 'SPR', 'SWF', 'SWS']
@@ -48,6 +48,7 @@ class Subjects(object):
 		'CRYPTO': 'https://github.com/apache/commons-crypto.git',
 		'CLI':'https://github.com/apache/commons-cli.git',
 		'NET':'https://github.com/apache/commons-net.git',
+		'IMAGING':'https://github.com/apache/commons-imaging.git',
 
 		'ENTESB': 'https://github.com/jboss-fuse/fuse.git',
 		'JBMETA': 'https://github.com/jboss/metadata.git',
