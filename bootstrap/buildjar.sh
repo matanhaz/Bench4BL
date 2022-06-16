bf6c0d6098ea737f28887de61c14d286a25b3fee
matanhaz <48633383+matanhaz@users.noreply.github.com>
2022-06-16 12:59:18 +0300

    Update buildjar.sh

--- bootstrap/buildjar.sh
+++ bootstrap/buildjar.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-cd `git rev-parse --show-toplevel`/src/Locus
-mvn package -DskipTests -fn
-cp target/Locus.jar ../../techniques
+# cd `git rev-parse --show-toplevel`/src/Locus
+# mvn package -DskipTests -fn
+# cp target/Locus.jar ../../techniques
 
 cd `git rev-parse --show-toplevel`/src/BugLocator
 mvn package  -DskipTests -fn
 cp target/BugLocator.jar ../../techniques
 
-cd `git rev-parse --show-toplevel`/src/AmaLgam
-mvn package  -DskipTests -fn
-cp target/AmaLgam.jar ../../techniques
+# cd `git rev-parse --show-toplevel`/src/AmaLgam
+# mvn package  -DskipTests -fn
+# cp target/AmaLgam.jar ../../techniques
 
-cd `git rev-parse --show-toplevel`/src/BLUiR
-mvn package  -DskipTests -fn
-cp target/BLUiR.jar ../../techniques
+# cd `git rev-parse --show-toplevel`/src/BLUiR
+# mvn package  -DskipTests -fn
+# cp target/BLUiR.jar ../../techniques
 
 cd `git rev-parse --show-toplevel`/src/BRTracer
 mvn package  -DskipTests -fn
